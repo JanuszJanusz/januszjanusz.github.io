80dea68c576b0f1be975bf70c347111d36cd446c
JanuszJanusz <31892726+JanuszJanusz@users.noreply.github.com>
2026-05-25 12:02:48 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+openContent = (evt, tabName) => {
+    let i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("links");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+document.getElementById("default").click();
